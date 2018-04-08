a2cedef09d9a9eb2ed0457ab4cfdf192b586c003
root <root@localhost.localdomain>
2018-04-08 18:35:08 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+//Definir las varibables de entorno IMPORTANTE
+app.set('port',(process.env.PORT||8080))
+
+//basic test
+app.get('/',function(req,res){
+	res.send("<h1>Test Heroku</h1>");
+})
+
+//TEST CON kali>node app.js
+app.listen(app.get('port'));
